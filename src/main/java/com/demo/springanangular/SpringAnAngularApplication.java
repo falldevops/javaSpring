578ce33e40bd41b3dd070c1be377f387e878d1aa
unknown <oumar.fall@octopeek.com>
2021-09-05 19:21:04 +0200

    first commit

---
+++ src/main/java/com/demo/springanangular/SpringAnAngularApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springanangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAnAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/springanangular/SpringAnAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springanangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
